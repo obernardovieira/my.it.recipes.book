50f6fb5a11fe9b78362e52c289cdad30b108f8c0
Bernardo Vieira <bernardo5@live.com.pt>
2017-11-06 08:47:45 +0000

    compile script updated

--- compile-book.sh
+++ compile-book.sh
@@ -8,4 +8,4 @@ makeglossaries $MYFILENAME
 makeindex $MYFILENAME-url
 pdflatex $MYFILENAME.tex
 pdflatex $MYFILENAME.tex
-ls | grep -v ".tex" | grep -v *.sh | grep -v LICENSE | grep -v *.md | grep -v *.pdf | grep -v Chapters | grep -v Figs | xargs rm
+ls | grep -v ".tex" | grep -v *.sh | grep -v LICENSE | grep -v *.md | grep -v *.pdf | grep -v *.jpg | grep -v Chapters | grep -v Figs | xargs rm
